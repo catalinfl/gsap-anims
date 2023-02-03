ef22695887cea15384093f083f6b8043b00d10cd
catalinfl <catalinfl02@gmail.com>
2023-02-03 14:26:43 +0200

    circle stagger

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,34 +1,41 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 import './Circle.scss'
-import gsap from "gsap"
+import gsap, {Power4} from "gsap"
 
 const Circle = () => {
 
+    const [reversed, setReversed] = useState<boolean>(true);
+    const app = useRef<HTMLDivElement>(null)
+    let t1 = useRef<GSAPTimeline | null>(null);
 
-
-
-    useEffect(() => {
-        gsap.to(circlesRef.current, {
-            rotate: 360,
-            opacity: 1,
-            duration: 3
+    useLayoutEffect(() => {
+      const ctx = gsap.context(() => {
+        t1.current && t1.current.progress(0).kill();
+        t1.current = gsap.timeline() 
+        .to(".circle", {
+          scale: 2,
+          stagger: 0.1
         })
+        .to(".circle", {
+          stagger: 0.25,
+          x: 100
+        })
+      }, app);
+      return () => ctx.revert()
     }, [])
 
-    const circlesRef = useRef<HTMLDivElement>(null);
-    const ref1 = useRef<HTMLDivElement>(null);
-    const ref2 = useRef<HTMLDivElement>(null);
-    const ref3 = useRef<HTMLDivElement>(null);
-    const ref4 = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      t1.current?.reversed(reversed)
+    }, [reversed])
+
+
 
   return (
-    <div className="circles"> 
-    <div ref={circlesRef} className="container-circle"> 
-    <div ref={ref1} className="circle"> </div>
-    <div ref={ref2} className="circle orange"> </div>
-    <div ref={ref3} className="circle blue"> </div>
-    <div ref={ref4} className="circle yellow"> </div>
-    </div>
+    <div className="container-circle" ref={app} onClick={() => setReversed(!reversed)}> 
+    <div className="circle"> t </div>
+    <div className="circle orange"> </div>
+    <div className="circle blue"> </div>
+    <div className="circle yellow"> </div>
     </div>
     )
 }
